8223f9ddadc6b89edd751a01fbea37338a16e802
Stephanie <stephanie.manriq@gmail.com>
2024-08-16 17:05:57 +0200

    refactoring env functions

--- srcs/global/global_init.c
+++ srcs/global/global_init.c
@@ -17,119 +17,53 @@ char *ft_strjoin_free(char *s1, char *s2, int free_str)
 	return (new_str);
 }
 
-void free_env_array(char **array)
+int populate_env_array(t_env *env_list, char **env_array)
 {
-	int i;
+	t_env	*current;
+	int			i;
+	char	*temp;
 
+	current = env_list;
 	i = 0;
-	while (array[i])
+	while (current)
 	{
-		free(array[i]);
-		i++;
+		env_array[i] = ft_strjoin(current->key, "=");
+		if (!env_array[i])
+			return (free_array(env_array), 0);
+		temp = ft_strjoin(env_array[i], current->value);
+		free(env_array[i]);
+		if (!temp)
+			return (free_array(env_array), 0);
+		env_array[i++] = temp;
+		current = current->next;
 	}
-	free(array);
+	env_array[i] = NULL;
+	return (1);
 }
 
 int	set_env_array(t_env *env_list, char ***env_array)
 {
-    t_env	*current;
-    int		i;
-    int		env_count;
+	t_env	*current;
+	int		env_count;
 
-    current = env_list;
-	i = 0;
+	current = env_list;
 	env_count = 0;
-    while (current)
-    {
-        env_count++;
-        current = current->next;
-    }
-
-    // Free the existing env_array if not NULL
-    if (*env_array)
-    {
-        free_array(*env_array);
-        *env_array = NULL;
-    }
-
-    // Allocate memory for the new env_array
-    *env_array = malloc(sizeof(char *) * (env_count + 1));
-    if (!*env_array)
-        return (0); // Indicate failure
-
-    // Populate the env_array
-    current = env_list;
-    while (current)
-    {
-        (*env_array)[i] = ft_strjoin(current->key, "=");
-        if (!(*env_array)[i])
-        {
-            free_array(*env_array);
-            return (0); // Indicate failure
-        }
-        char *temp = ft_strjoin_free((*env_array)[i], current->value, 1);
-        if (!temp)
-        {
-            free((*env_array)[i]);
-            free_array(*env_array);
-            return (0); // Indicate failure
-        }
-        (*env_array)[i] = temp;
-        i++;
-        current = current->next;
-    }
-    (*env_array)[i] = NULL; // Null-terminate the array
-
-    return (1); // Indicate success
-}
-
-
-// void	set_env_array(t_env *env_list, char ***env_array)
-// {
-// 	t_env	*current;
-// 	int		i;
-
-// 	current = env_list;
-// 	i = 0;
-// 	while (current)
-// 	{
-// 		i++;
-// 		current = current->next;
-// 	}
-// 	if(!*env_array)
-// 	{
-// 		free_array(*env_array);
-// 		*env_array = NULL;
-// 	}
-// 	*env_array = malloc(sizeof(char *) * (i + 1));
-// 	if (!*env_array)
-// 		exit_shell("Error: malloc failed\n", EXIT_FAILURE);
-// 	i = 0;
-// 	current = env_list;
-// 	while (current)
-// 	{
-// 		(*env_array)[i] = ft_strjoin(current->key, "=");
-// 		if (!(*env_array)[i])
-// 			exit_shell("Error: malloc failed\n", EXIT_FAILURE);
-// 		(*env_array)[i] = ft_strjoin_free((*env_array)[i], current->value, 1);
-// 		if (!(*env_array)[i])
-// 			exit_shell("Error: malloc failed\n", EXIT_FAILURE);
-// 		i++;
-// 		current = current->next;
-// 	}
-// 	(*env_array)[i] = NULL;
-// }
-
-void print_array(char **array)
-{
-	int i;
-
-	i = 0;
-	while (array[i])
+	while (current)
 	{
-		printf("%s\n", array[i]);
-		i++;
+		env_count++;
+		current = current->next;
 	}
+	if (*env_array)
+	{
+		free_array(*env_array);
+		*env_array = NULL;
+	}
+	*env_array = malloc(sizeof(char *) * (env_count + 1));
+	if (!*env_array)
+		return (0);
+	if(populate_env_array(env_list, *env_array))
+		return (1);
+	return (0);
 }
 
 void	global_init(t_global **global, char **env)
