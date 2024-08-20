2b57001834743c9d50a79b2124536c215c1f6920
Henrizz <henriettezimmermann@hotmail.de>
2024-08-20 17:21:48 +0200

    fixed in codespaces

--- srcs/get_input/parse_syntax_utils.c
+++ srcs/get_input/parse_syntax_utils.c
@@ -6,7 +6,7 @@
 /*   By: Henriette <Henriette@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 13:12:16 by hzimmerm          #+#    #+#             */
-/*   Updated: 2024/08/17 20:39:37 by Henriette        ###   ########.fr       */
+/*   Updated: 2024/08/20 17:17:09 by Henriette        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 int	syntax_check(char *str)
 {
 	str = NULL;
+	(void)str;
 	return (0);
 	//when symbols are next to | --> syntax error near unexpected token
-	//when 
-	// when symbols other than | > < are there when not inside quotes 
+	//when symbols other than | > < are there when not inside quotes 
+	//in case of error return 1
 }
